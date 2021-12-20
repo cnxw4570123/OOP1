c0fe334bb92a7a97066b8c97733e41af4e3b20b7
cnxw4570123 <cnxw4570123@gmail.com>
2021-12-20 12:18:14 +0900

    week03 hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main() {
+	std::cout << "계절학기 OOP1" << std::endl;
+	std::cout << "to show the main  structure." << std::endl;
+	std::cout << "We learn more about the language " << std::endl;
+	std::cout << "in this chapter and the rest of the book." << std::endl;
+}
